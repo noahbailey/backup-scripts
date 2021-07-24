af2b929160e28a4f7a47af2134ef15796c6af97b
Noah Bailey <noah@nbailey.ca>
2021-07-24 10:00:13 -0400

    Cleanup nextcloud script, remove verbose output

---
+++ nextcloud/backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SERVER="beastie.intranet.nbailey.ca"
+
+## Backup the database
+NOW=$(date +"%Y-%m-%d-%H-%M-%S")
+mysqldump nextcloud | gzip > /opt/mysql-backups/nextcloud-$NOW.sql.gz
+find /opt/mysql-backups/ -mtime +7 -delete
+
+# Sync database backups to server
+rsync -azh --delete /opt/mysql-backups/ ncbackup@"$SERVER":/tank/nextcloud/database/
+
+# Sync nextcloud directory to server
+rsync -azh --delete /var/www/nextcloud  ncbackup@"$SERVER":/tank/nextcloud/backup/
--- nextcloud/nextcloud-backup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rsync -azh --delete  --progress /var/www/nextcloud ncbackup@beastie.intranet.nbailey.ca:/tank/nextcloud/backup/
