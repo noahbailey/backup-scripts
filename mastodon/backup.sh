78702cde0721a7faa13ca51f99d951e08540e5ba
Noah Bailey <noah@nbailey.ca>
2022-11-24 21:53:41 -0500

    Fixup shellcheck issues

--- mastodon/backup.sh
+++ mastodon/backup.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 NOW=$(date +"%Y-%m-%d-%H-%M-%S")
 SERVER="beastie.intranet.nbailey.ca"
 
-pg_dump mastodon_production | gzip > /home/mastodon/backups/mastodon_production-$NOW.sql.gz
+pg_dump mastodon_production | gzip > /home/mastodon/backups/mastodon_production-"$NOW".sql.gz
 find /home/mastodon/backups/ -type f -mtime +14 -delete
 
-rsync -azh --delete /home/mastodon/ mastobackup@beastie.intranet.nbailey.ca:/tank/mastodon/data/
+rsync -azh --delete /home/mastodon/ mastobackup@"$SERVER":/tank/mastodon/data/
