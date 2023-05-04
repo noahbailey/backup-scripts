d926a98bdb194f73dd58b7d3a1ef28a230233c8d
Noah Bailey <noah@nbailey.ca>
2023-05-04 19:27:55 -0400

    Update nextcloud backup script

--- nextcloud/backup.sh
+++ nextcloud/backup.sh
@@ -4,11 +4,17 @@ SERVER="beastie.intranet.nbailey.ca"
 
 ## Backup the database
 NOW=$(date +"%Y-%m-%d-%H-%M-%S")
+echo "--- Starting backup at $NOW ---" >> /tmp/backup.log
+echo "Destination: $SERVER" >> /tmp/backup.log
+
 mysqldump nextcloud | gzip > /opt/mysql-backups/nextcloud-"$NOW".sql.gz
 find /opt/mysql-backups/ -mtime +7 -delete
 
 # Sync database backups to server
-rsync -azh --delete /opt/mysql-backups/ ncbackup@"$SERVER":/tank/nextcloud/database/
+rsync -azhi --delete /opt/mysql-backups/ ncbackup@"$SERVER":/tank/nextcloud/database/ >> /tmp/backup.log
 
 # Sync nextcloud directory to server
-rsync -azh --delete /var/www/nextcloud  ncbackup@"$SERVER":/tank/nextcloud/backup/
+rsync -azhi --delete /var/www/nextcloud  ncbackup@"$SERVER":/tank/nextcloud/backup/ >> /tmp/backup.log
+
+# Finish backup run
+echo "--- Done backup at $NOW ---" >> /tmp/backup.log
